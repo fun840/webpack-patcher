0a18d583578212eb8250b791e461423c347715c5
fun840 <fun840@protonmail.com>
2023-06-04 22:15:49 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Webpack Patcher
+
+This is a small userscript I made for patching Webpack. It's mostly intended to be used as a base for other scripts. This userscript was coded with Webpack 4 (`webpackJsonp`) in mind, but it should still work for Webpack 5.
+
+Thanks to [Vendicated](https://github.com/Vendicated/) for some help with Webpack internals.
---
+++ WebpackPatcher.user.js
@@ -0,0 +1,64 @@
+// ==UserScript==
+// @name        Webpack Patcher
+// @author      fun840 (https://github.com/fun840)
+// @namespace   https://github.com/fun840/webpack-patcher
+// @version     1.0
+// @match       https://example.com/
+// @grant       none
+// @description A small userscript for patching webpack
+// @run-at      document-start
+// ==/UserScript==
+
+const webpackArray = "webpackJsonp"; // the webpack chunk array
+const log = true; // disable if you don't want messages for patching every module
+
+const patches = [
+    /* example patch
+    {
+        // this can be regex or a string
+        from: "function foo() {",
+        to: "function foo(){console.log('Successfully patched!')",
+    },
+    */
+];
+
+// check if eval() is allowed, if it's not the script won't work
+try {
+    eval("");
+} catch {
+    throw new Error("[Webpack Patcher] Eval is unavailable, script won't work!");
+}
+
+Object.defineProperty(window, webpackArray, {
+    set(val) {
+        val.push = new Proxy(val.push, {
+            apply(target, thisArg, args) {
+                const arr = args[0];
+                const modules = arr[1];
+
+                for (const id of Object.keys(modules)) {
+                    let func = modules[id].toString();
+
+                    const oldFunc = func;
+                    for (const patch of patches) {
+                        func = func.replace(patch.from, patch.to);
+                    }
+
+                    // don't re-eval if not needed
+                    if (func != oldFunc) {
+                        modules[id] = eval("0," + func);
+                    }
+                }
+
+                log && console.log("[Webpack Patcher] Patched", modules);
+
+                return target.apply(thisArg, args);
+            },
+        });
+
+        this.value = val;
+    },
+    get() {
+        return this.value;
+    },
+});
